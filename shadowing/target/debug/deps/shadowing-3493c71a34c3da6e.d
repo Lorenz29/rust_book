b8451f3c408a20c31a11543ac03cae31c18ee32b
Lorenz29 <lorenzohardoy@hotmail.com>
2024-08-09 10:38:32 -0300

    shadowing

---
+++ shadowing/target/debug/deps/shadowing-3493c71a34c3da6e.d
@@ -0,0 +1,5 @@
+/home/lorenz29/projects/rust_book/shadowing/target/debug/deps/shadowing-3493c71a34c3da6e: src/main.rs
+
+/home/lorenz29/projects/rust_book/shadowing/target/debug/deps/shadowing-3493c71a34c3da6e.d: src/main.rs
+
+src/main.rs:
---
+++ shadowing/target/debug/shadowing.d
@@ -0,0 +1,1 @@
+/home/lorenz29/projects/rust_book/shadowing/target/debug/shadowing: /home/lorenz29/projects/rust_book/shadowing/src/main.rs
